e98a06c46b52a1a9024ea00340bd10e0f0ce8516
wfc <39686870+weifucheng1102@users.noreply.github.com>
2023-09-22 15:18:43 +0800

    Update carnumber_bottom_dialog.dart

--- lib/car_number/carnumber_bottom_dialog.dart
+++ lib/car_number/carnumber_bottom_dialog.dart
@@ -204,7 +204,10 @@ class _CarNumberKeyViewState extends State<CarNumberKeyView> {
             height: double.infinity,
             width: double.infinity,
             child: text == mDelete ?
-            Image.asset("lib/images/icon_rollback.png" , width: 23 , height: 17,):
+            Text('删除', style:
+              TextStyle(color: text == 'I' ? Color(0XFFB7BABC) : color_00131D
+                  , fontSize: (18)),
+            ):
             Text(text, style:
               TextStyle(color: text == 'I' ? Color(0XFFB7BABC) : color_00131D
                   , fontSize: (18)),
